01be6827c3e3d667cf981fb74105f0b4428c45f8
heitorbarbieri <superheitor@gmail.com>
2025-07-01 10:18:45 -0300

    version 1.13.7 super abstracts text area now has style

--- src/main/scala/org/bireme/dmf/DMFServlet.scala
+++ src/main/scala/org/bireme/dmf/DMFServlet.scala
@@ -450,6 +450,28 @@ class DMFServlet extends HttpServlet {
       #textWithTooltipsAnnif a {
         cursor: pointer;
       }
+
+       #superResumos {
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: .25rem;
+        transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+        overflow-x: auto;
+      }
+      #textWithTooltips:focus-visible {
+        background-color: #fff;
+        border-color: #80bdff;
+        outline: 0;
+        box-shadow: 0 0 0 .2rem rgba(0, 123, 255, .25);
+      }
+      #textWithTooltips a {
+        cursor: pointer;
+      }
     </style>
 
     <style>
